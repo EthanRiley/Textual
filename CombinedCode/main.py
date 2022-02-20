eb2698bd00f0e72009a500bb3ac4beba47179502
Ethan Gilworth <49215273+EthanRiley@users.noreply.github.com>
2022-02-19 21:13:57 -0500

    started main and some funtional changes

---
+++ CombinedCode/main.py
@@ -0,0 +1,11 @@
+from Textual import textual
+from TextualParsers import 
+
+def main():
+    drake_songs = ['cant_take_a_joke.txt', 'dont_matter_to_me.txt', 'nine.txt', 'nonstop.txt', 'redemption.txt', 'views.txt']
+    drake_text = ''
+    for song in drake_songs:
+        pass
+    kanye_songs = ['all_falls_down.txt', 'jesus_walks.txt', 'dark_fantasy.txt', 'never_let_me_down.txt', 'power.txt', 'runaway.txt']
+    kendrick_songs = ['alright.txt', 'blacker_the_berry.txt', 'dont_kill_my_vibe.txt', 'king_kunta.txt', 'maad_city.txt', 'money_trees.txt']
+    kendrick_text = ''
